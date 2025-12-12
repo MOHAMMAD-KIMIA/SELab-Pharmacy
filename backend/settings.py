8204df8d5784df00db5ebbc77734e2355bcb5ff2
MOHAMMAD-KIMIA <amir.m.kimia@gmail.com>
2025-12-12 20:47:27 +0330

    REST_FRAMEWORK in setting

--- backend/settings.py
+++ backend/settings.py
@@ -125,3 +125,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+}
