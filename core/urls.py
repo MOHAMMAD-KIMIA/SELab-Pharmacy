a9aff37a6e9c4f0032d994546999a23671299472
MOHAMMAD-KIMIA <amir.m.kimia@gmail.com>
2025-12-12 15:18:12 +0330

    set API of prescriptions

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import home, signup, login, MedicineViewSet
+from .views import home, signup, login, MedicineViewSet, PrescriptionViewSet
 
 router = DefaultRouter()
 router.register('medicines', MedicineViewSet, basename='medicines')
+router.register('prescriptions', PrescriptionViewSet, basename='prescriptions')
 
 urlpatterns = [
     path('', home),
