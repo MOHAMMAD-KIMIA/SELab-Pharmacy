38a455ab8f2d20ee0a7af6af017018b1001d63d7
MOHAMMAD-KIMIA <amir.m.kimia@gmail.com>
2026-01-30 13:32:54 +0330

    ADD somthing

--- config/urls.py
+++ config/urls.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def well_known_chrome_devtools_json(_request):
+    return HttpResponse(status=204)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("core.urls")),
-] 
-
-urlpatterns = [
+    path(
+        ".well-known/appspecific/com.chrome.devtools.json",
+        well_known_chrome_devtools_json,
+    ),
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
